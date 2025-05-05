31431d85d63f2b423085f6eb2a6c3c703f353f1e
Sebastian Kühnau <seb@vaadin.com>
2025-05-05 16:37:15 +0200

    add grid and fetch data from backend

--- src/main/java/com/svenruppert/expedition/planner/views/tour/TourView.java
+++ src/main/java/com/svenruppert/expedition/planner/views/tour/TourView.java
@@ -1,8 +1,16 @@
 package com.svenruppert.expedition.planner.views.tour;
 
+import com.svenruppert.expedition.planner.data.entity.Participant;
+import com.svenruppert.expedition.planner.data.entity.Tour;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.stream.Collectors;
+
+import static com.svenruppert.expedition.planner.services.SingletonRegistry.getOrCreateTourService;
 
 @Route("tours")
 public class TourView extends VerticalLayout implements HasDynamicTitle {
@@ -13,13 +21,27 @@ public class TourView extends VerticalLayout implements HasDynamicTitle {
      *    - extend from component class
      *    - add link to menu in MainLayout
      * 2. - set up dynmic title
+     * 3. - add grid and fetch data from backend
      */
 
     public static final String MENU_ITEM_TOUR = "mainlayout.menuitem.tour";
     public static final String TITLE = "tour.title";
 
     public TourView() {
+        var tourService = getOrCreateTourService();
 
+        var grid = new Grid<>(Tour.class);
+        grid.setColumns("name");
+        grid.addComponentColumn(tour -> {
+                var partipantsList = tour.getParticipantSet().stream()
+                        .map(Participant::getName)
+                        .collect(Collectors.joining(", "));
+                return new Span(partipantsList);
+            })
+                .setHeader(getTranslation("tour.participants"))
+                .setSortable(true);
+        grid.setItems(tourService.all());
+        add(grid);
     }
 
     @Override
